98b069703cb213d4a8ca95ae84aedb79bf034847
Efe Ozturkoglu <efeecehanozturkoglu@yahoo.com>
2018-02-27 00:40:12 -0500

    Created Polygon class.

---
+++ RenderSharp/Library/Polygon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderSharp.Library
+{
+    class Polygon
+    {
+        private Matrix[] vertices;
+        public Matrix[] Vertices {
+            get {
+                return vertices;
+            }
+            set {
+                vertices = value;
+            }
+        }
+
+        public Polygon()
+        {
+
+        }
+    }
+}
